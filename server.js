564d72f1a5be0d8ade46c0712c171d721aa82467
sahil2796 <sahilwork2796@gmail.com>
2019-11-25 01:49:41 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config({ path: 'process.env' })
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database '))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json({limit: '10mb'}))
+
+app.use(express.json())
+
+const movieRouter = require('./routes/movies')
+app.use('/movies', movieRouter)
+
+
+app.listen(3000, () => console.log('Server Started'))
